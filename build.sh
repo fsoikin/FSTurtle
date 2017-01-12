99152f9af19220a2031b51dff68001e83e7ae0c9
Fyodor Soikin <name.fa@gmail.com>
2017-01-12 15:30:09 -0500

    Revert back to Mono on Travis.

--- build.sh
+++ build.sh
@@ -16,18 +16,18 @@ then
   
   packages/build/FAKE/tools/FAKE.exe $@ --fsiargs build.fsx 
 else
-  # use dotnet
-  dotnet .paket/paket.bootstrapper.exe
+  # use mono
+  mono .paket/paket.bootstrapper.exe
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
   	exit $exit_code
   fi
 
-  dotnet .paket/paket.exe restore
+  mono .paket/paket.exe restore
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
   	exit $exit_code
   fi
 
-  dotnet packages/build/FAKE/tools/FAKE.exe $@ --fsiargs build.fsx 
+  mono packages/build/FAKE/tools/FAKE.exe $@ --fsiargs build.fsx 
 fi
