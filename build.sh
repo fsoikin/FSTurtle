b69a9d251e243092def6d262e3e38b0d0e249768
Fyodor Soikin <name.fa@gmail.com>
2017-01-12 12:52:31 -0500

    Trying out .NET Core on Travis.

--- build.sh
+++ build.sh
@@ -17,13 +17,6 @@ then
   packages/build/FAKE/tools/FAKE.exe $@ --fsiargs build.fsx 
 else
   # use dotnet
-  export DOTNET_SDK_URL="https://go.microsoft.com/fwlink/?LinkID=809129"
-  export DOTNET_INSTALL_DIR="$PWD/.dotnetcli"
-  mkdir $DOTNET_INSTALL_DIR
-  curl -L $DOTNET_SDK_URL -o dotnet_package
-  tar -xvzf dotnet_package -C $DOTNET_INSTALL_DIR
-  export PATH="$DOTNET_INSTALL_DIR:$PATH"
-
   dotnet .paket/paket.bootstrapper.exe
   exit_code=$?
   if [ $exit_code -ne 0 ]; then
